6715d65821e2a17001ff7896771d58f0fe19df75
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2024-01-19 22:26:57 +0100

    started php

--- 20200110/compito_A/PHP/index.php
+++ 20200110/compito_A/PHP/index.php
@@ -1,4 +1,35 @@
 <?php
+    $db = new mysqli("localhost", "root", "", "climate");
+    if ($db->connect_error) {
+        die("Errore: ". $db->connect_error);
+    }
+    $query = "SELECT DISTINCT citta from temperature";
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+    $cities = array();
+    foreach($result as $city) {
+        $cities[] = $city["citta"];
+    }
+    if($_SERVER['REQUEST_METHOD'] == 'GET') {
+        $selectedCity = $_GET['citta'];
+        $query = "SELECT max FROM temperature WHERE citta = ?";
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("s", $selectedCity);
+        $stmt->execute();
+        $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        $max = $result[0]["max"];
+
+        $query = "SELECT min FROM temperature WHERE citta = ?";
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("s", $selectedCity);
+        $stmt->execute();
+        $result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+        $min = $result[0]["min"];
+        echo "<p>Città selezionata: ".$selectedCity."</p>";
+        echo "<p>Temperatura massima: ".$max."</p>";
+        echo "<p>Temperatura minima: ".$min."</p>";
+    }
 ?>
 
 <html lang="it">
@@ -10,6 +41,11 @@
     <form action="index.php" method="GET">
       <label for="citta">Città</label>
       <select name="citta">
+        <?php
+            foreach($cities as $city) {
+                echo "<option value=\"".$city."\">".$city."</option>";
+            }
+        ?>
       </select>
       <input type="submit" value="Invia" />
     </form>
